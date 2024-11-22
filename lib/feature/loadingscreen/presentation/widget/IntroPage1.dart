b7a08a3ead7c366490c1ec89ad10a492796857cc
Argiee <112626873+Argiezuu@users.noreply.github.com>
2024-11-22 23:10:22 +0800

    applying animation on loadingscreen

--- lib/feature/loadingscreen/presentation/widget/IntroPage1.dart
+++ lib/feature/loadingscreen/presentation/widget/IntroPage1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:swine_care/colors/colors.dart';
 
 class IntroPage1 extends StatelessWidget {
@@ -6,10 +7,28 @@ class IntroPage1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: appGreen5,
-      child: Center(
-        child: Text("BOMBAY"),
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Center(
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("HAHAHAHAHHA",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: appBlack
+            ),),
+
+            SizedBox(
+              width: screenWidth * 0.8,
+              height: screenHight * 0.4,
+              child: Lottie.asset("assets/Animations/AnimationPigDance.json"),
+            )
+          ],
+        )
       ),
     );
   }
--- lib/feature/loadingscreen/presentation/widget/IntroPage3.dart
+++ lib/feature/loadingscreen/presentation/widget/IntroPage3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:swine_care/colors/colors.dart';
 
 class IntroPage3 extends StatelessWidget {
@@ -6,10 +7,27 @@ class IntroPage3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: appPink,
-      child: Center(
-        child: Text("AAAAAAAAAAAAAAAAAA"),
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screemHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("ratatowe",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+                color: appBlack
+            ),),
+            SizedBox(
+              width: screenWidth * 0.8,
+              height: screemHeight * 0.4,
+              child: Lottie.asset("assets/Animations/AnimationWomen.json"),
+            ),
+          ],
+        )
       ),
     );
   }
