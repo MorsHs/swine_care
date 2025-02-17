e6b99873f316d35cf07bfc32686db2c119b54ff8
argieuwu <argiepardillo009@gmail.com>
2025-02-18 00:41:15 +0800

    enhancing IntroPage.dart and loadingscreen pages by adding some loading effect and background images

--- lib/feature/intro/presentation/pages/IntroPage.dart
+++ lib/feature/intro/presentation/pages/IntroPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 
 class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
@@ -12,7 +13,7 @@ class _IntroPageState extends State<IntroPage> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(milliseconds: 800), () {
+    Future.delayed(const Duration(milliseconds: 3000), () {
       context.go('/loading-screen');
     });
   }
@@ -20,12 +21,29 @@ class _IntroPageState extends State<IntroPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
-        child: Image.asset(
-          "Assets/Logo/swinecarelogo.png",
-          fit: BoxFit.contain,
-          height: 160,
-          width: 160,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              "Assets/Logo/swinecarelogo.png",
+              fit: BoxFit.contain,
+              height: 180,
+              width: 180,
+            ),
+            const SizedBox(height: 90),
+            const SizedBox(
+              width: 30,
+              height: 30,
+              child: LoadingIndicator(
+                indicatorType: Indicator.ballBeat,
+                colors: [Colors.blueAccent],
+                strokeWidth: 2,
+                backgroundColor: Colors.transparent,
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/feature/loadingscreen/presentation/widget/IntroPage1.dart
+++ lib/feature/loadingscreen/presentation/widget/IntroPage1.dart
@@ -10,47 +10,54 @@ class IntroPage1 extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
-    return Container(
-      padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              "Welcome to",
-              style: GoogleFonts.saira(
-                  fontSize: 30,
-                  // fontWeight: FontWeight.bold
-              ),
+    return Stack(
+      children: [
+        Positioned.fill(
+            child: Image.asset(
+                'assets/images/loadingscreenbackground/background6.jpg',
+            fit: BoxFit.cover,
             ),
-            Text(
-              "SwineCare",
-              style: GoogleFonts.saira(
-                  fontSize: 50,
-                  fontWeight:FontWeight.bold
-              ),
-            ),
-            SizedBox(
-              width: screenWidth * 0.8,
-              height: screenHeight * 0.4,
-              child: Lottie.asset("assets/Animations/AnimationPigDance.json"),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: Text(
-                "Your ultimate AI-powered assistant for swine health! "
-                    "SwineCare helps you monitor, diagnose, and make smart decisions to ensure your pigs stay healthy and disease-free.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  height: 1.5,
-                  fontSize: 18,
-                  fontStyle: FontStyle.italic,
-                ),
-              ),
-            ),
-          ],
         ),
-      ),
+        Container(
+          padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: screenWidth * 0.8,
+                  height: screenHeight * 0.4,
+                  child:
+                      Lottie.asset("assets/Animations/AnimationPigDance.json"),
+                ),
+                Text(
+                  "Welcome to SwineCare",
+                  style: GoogleFonts.saira(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
+                  ),
+                ),
+                SizedBox(height: 10),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Text(
+                    "Your ultimate AI-powered assistant for swine health! "
+                    "SwineCare helps you monitor, diagnose, and make smart decisions to ensure your pigs stay healthy and disease-free.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      height: 1.5,
+                      fontSize: 16,
+                      fontStyle: FontStyle.italic,
+                        color: Colors.black
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
