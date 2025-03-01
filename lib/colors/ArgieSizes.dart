9d0fd4da9470dca94ff666a00d52a4c0722ab70f
argieuwu <argiepardillo009@gmail.com>
2025-03-01 22:32:50 +0800

    providing a component to use and adding a icon dependency

---
+++ lib/colors/ArgieSizes.dart
@@ -0,0 +1,24 @@
+
+class ArgieSizes{
+  ArgieSizes._();
+
+
+
+//Button sizes
+static  double buttonHeight = 18.0;
+static  double buttonRadius = 12.0;
+static  double buttonWidth = 120.0;
+static  double buttonElevation = 4.0;
+
+//AppBar height
+static double appBarHeight = 56.0;
+
+//default spacing between section
+static double defaultSpace = 24.0;
+static  double spaceBwtItem = 16.0;
+static  double spaceBwtSection = 32.0;
+
+//divider height
+static  double dividerHeight = 1.0;
+
+}
